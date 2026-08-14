a3278808994192d93708a5a4461c8eab38eb5de1
aaronquan1 <aaron.quan@cae.com>
2026-08-14 21:52:50 +1000

    texture interface

--- src/WebGL/Texture/texture.ts
+++ src/WebGL/Texture/texture.ts
@@ -13,107 +13,137 @@ const EmptyFunction = () => {};
 
 interface GenericTexture{
   load: () => void;
-  active: (id: Int32) => void;
+  active: (id: Int32) => boolean;
+  isLoaded: () => boolean;
 }
 
 //canvas texture
 export class CanvasTexture implements GenericTexture{
-  load(){
-
+  texture: WebGLTexture | undefined;
+  img_data: ImageData | undefined;
+  private is_loaded: boolean;
+  constructor(){
+    this.is_loaded = false;
   }
-  active(id: Int32){
+  setImageData(data: ImageData){
+    this.img_data = data;
+  }
+  load(){
+    const gl = WebGL.gl;
+    if(gl != undefined && this.img_data != undefined && !this.is_loaded){
+      this.texture = gl.createTexture();
+      gl.bindTexture(gl.TEXTURE_2D, this.texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.img_data);
 
+      this.img_data = undefined; // if bugged, comment out this line, texture might need data pointer
+      this.is_loaded = true;
+    }
+  }
+  active(id: Int32): boolean{
+    const gl = WebGL.gl;
+    if(this.texture && this.is_loaded && gl != undefined){
+      gl.activeTexture(gl.TEXTURE0+id);
+      gl.bindTexture(gl.TEXTURE_2D, this.texture);
+      return true;
+    }
+    return false;
+  }
+  isLoaded(){
+    return this.is_loaded;
   }
   
 }
 
 //URL texture
 export class Texture implements GenericTexture{
-    static setup(){
-      //not needed? should be set after each texture load?
-      /*
-      if(WebGL.gl){
-        const gl = WebGL.gl;
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-      }*/
-    }
-    static createTextureFromUrl(url: string): Texture{
-      const img = new Image();
-      img.src = url;
-      this.textures_requested++;
+  static setup(){
+    //not needed? should be set after each texture load?
+    /*
+    if(WebGL.gl){
       const gl = WebGL.gl;
-      if(gl){
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    }*/
+  }
+  static createTextureFromUrl(url: string): Texture{
+    const img = new Image();
+    img.src = url;
+    this.textures_requested++;
+    const gl = WebGL.gl;
+    if(gl){
+      img.onload = () => {
+        if(WebGL.gl){
+          const gl = WebGL.gl;
+          const texture = gl.createTexture();
+          gl.bindTexture(gl.TEXTURE_2D, texture);
+          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+          this.textures_loaded++;
+        }
+      }
+    }
+    return new Texture(url);
+  }
+
+  static textures_loaded: Int32 = 0;
+  static textures_requested: Int32 = 0;
+  static textures_failed: Set<string> = new Set();
+  static path = "/";
+
+
+  texture: WebGLTexture | undefined;
+  is_loaded: boolean;
+  url: string
+  constructor(fn: string){
+    //const gl = WebGL.gl;
+    this.url = Texture.path+fn;
+    this.is_loaded = false;
+  }
+  load(onLoad:VoidFunction=EmptyFunction, onError:ErrorFunction=EmptyErrorFunction){
+    console.log(this.url);
+    if(!this.is_loaded){
+      Texture.textures_requested++;
+      const gl = WebGL.gl;
+      if(gl != undefined){
+        const img = new Image();
+        img.src = this.url;
         img.onload = () => {
-          if(WebGL.gl){
-            const gl = WebGL.gl;
-            const texture = gl.createTexture();
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-            this.textures_loaded++;
-          }
+          console.log(`Loaded texture ${this.url}`);
+          this.texture = gl.createTexture();
+          gl.activeTexture(gl.TEXTURE0);
+          gl.bindTexture(gl.TEXTURE_2D, this.texture);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+          Texture.textures_loaded++;
+          this.is_loaded = true;
+          onLoad();
+        }
+        img.onerror = (e) => {
+          onError(e);
         }
       }
-      return new Texture(url);
+    }else{
+      onLoad();
+    }
+  }
+  //static loadList
+  active(id: Int32): boolean{
+    const gl = WebGL.gl;
+    if(this.texture && this.is_loaded && gl != undefined){
+      gl.activeTexture(gl.TEXTURE0+id);
+      gl.bindTexture(gl.TEXTURE_2D, this.texture);
+      return true;
     }
 
-    static textures_loaded: Int32 = 0;
-    static textures_requested: Int32 = 0;
-    static textures_failed: Set<string> = new Set();
-    static path = "/";
-
-
-    texture: WebGLTexture | undefined;
-    is_loaded: boolean;
-    url: string
-    constructor(fn: string){
-      //const gl = WebGL.gl;
-      this.url = Texture.path+fn;
-      this.is_loaded = false;
-    }
-    load(onLoad:VoidFunction=EmptyFunction, onError:ErrorFunction=EmptyErrorFunction){
-      console.log(this.url);
-      if(!this.is_loaded){
-        Texture.textures_requested++;
-        const gl = WebGL.gl;
-        if(gl != undefined){
-          const img = new Image();
-          img.src = this.url;
-          img.onload = () => {
-            console.log(`Loaded texture ${this.url}`);
-            this.texture = gl.createTexture();
-            gl.activeTexture(gl.TEXTURE0);
-            gl.bindTexture(gl.TEXTURE_2D, this.texture);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-            Texture.textures_loaded++;
-            this.is_loaded = true;
-            onLoad();
-          }
-          img.onerror = (e) => {
-            onError(e);
-          }
-        }
-      }else{
-        onLoad();
-      }
-    }
-    //static loadList
-    active(id: Int32){
-      const gl = WebGL.gl;
-      if(this.texture && this.is_loaded && gl != undefined){
-        gl.activeTexture(gl.TEXTURE0+id);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        return true;
-      }
-
-      return false;
-    }
+    return false;
+  }
+  isLoaded(): boolean{
+    return this.is_loaded;
+  }
 }
 
 export class TextureCollection{
